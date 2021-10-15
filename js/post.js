0d3c441a118920f27404138efe39774633c15fbf
MawatariTanaka <77230280+MawatariTanaka@users.noreply.github.com>
2021-10-15 23:16:12 +0700

    2021 - 10 - 15
    Add about site

--- js/post.js
+++ js/post.js
@@ -1,10 +1,10 @@
 /// Post control
 
-function moveBack(){
+function moveBack() {
     document.location.href = "../blog.html";
 }
 
-function postControl(n){
+function postControl(n) {
     document.getElementById("selected-post").innerHTML = posts[n].paragraph;
     document.getElementById("selected-h2").innerHTML = posts[n].title;
     changeBackground(document.getElementById("selected-main-background"), n);
@@ -14,6 +14,57 @@ postControl(window.localStorage.getItem('postId'));
 
 /// Slide show
 
+// Add content to slide show
+let postIndex = [];
+for (let post of posts) {
+    postIndex.push(post.id);
+}
+
+let showedPost = [];
+for (let i = 0; i < 3; i++) {
+    let n = Math.floor(Math.random() * postIndex.length);
+    showedPost.push(postIndex[n]);
+    postIndex.splice(n, 1);
+}
+
+document.getElementById("recent-posts-slideshow").innerHTML = `
+    <div class="recent-post fade">
+    <div class="post-pic" id="post-pic-${showedPost[0]}"></div>
+    <div class="post-h2" id="post-h2-${showedPost[0]}"></div>
+    </div>
+    <div class="recent-post fade">
+    <div class="post-pic" id="post-pic-${showedPost[1]}"></div>
+    <div class="post-h2" id="post-h2-${showedPost[1]}"></div>
+    </div>
+    <div class="recent-post fade">
+    <div class="post-pic" id="post-pic-${showedPost[2]}"></div>
+    <div class="post-h2" id="post-h2-${showedPost[2]}"></div>
+    </div>
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+`;
+
+document.getElementById("recent-posts-nav").innerHTML = `
+    <div class="recent-post-a">
+        <div class="post-pic-a" id="post-big-pic-${showedPost[0]}"></div>
+        <div class="post-h2-a" id="post-big-h2-${showedPost[0]}"></div>
+    </div>
+    <div class="recent-post-a">
+        <div class="post-pic-a" id="post-big-pic-${showedPost[1]}"></div>
+        <div class="post-h2-a" id="post-big-h2-${showedPost[1]}"></div>
+    </div>
+    <div class="recent-post-a">
+        <div class="post-pic-a" id="post-big-pic-${showedPost[2]}"></div>
+        <div class="post-h2-a" id="post-big-h2-${showedPost[2]}"></div>
+    </div>
+`
+
+for (let i = 0; i < 3; i++) {
+    changeBackground(document.getElementById(`post-pic-${showedPost[i]}`), showedPost[i]);
+    changeBackground(document.getElementById(`post-big-pic-${showedPost[i]}`), showedPost[i]);
+    document.getElementById(`post-h2-${showedPost[i]}`).innerHTML = posts[showedPost[i]].title;
+    document.getElementById(`post-big-h2-${showedPost[i]}`).innerHTML = posts[showedPost[i]].title;
+}
 // Add random posts to slide show
 
 
